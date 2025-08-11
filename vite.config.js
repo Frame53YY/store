12d2d7984f9a14da16fab71e21300a918e05c032
Oleksii Kovsh <kovsh1999@gmail.com>
2025-08-11 09:41:46 +0300

    fix svg

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import SortCss from 'postcss-sort-media-queries';
 
 export default defineConfig(({ command }) => {
   return {
+    base: '/store/',
     define: {
       [command === 'serve' ? 'global' : '_global']: {},
     },
